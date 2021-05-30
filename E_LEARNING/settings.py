dc77f0125cc56d28aff66a0136a1bc078ed50a8d
piyush-mishra-01 <73028990+piyush-mishra-01@users.noreply.github.com>
2021-05-30 11:16:01 +0530

    deploy

--- E_LEARNING/settings.py
+++ E_LEARNING/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!ll0dow0#w7li)j797ve97x!s3tw6ec(r294&s3%e&nuqn72&*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['e-learningport.herokuapp.com']
 
 
 # Application definition
